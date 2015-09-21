aea648f67c0f4dff3dfc8c5e941cd52375e6ad15
Shin-nosukeSaito <Shin-nosukeSaito>
2015-09-21 16:17:14 +0900

    korehasyakei

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+require('crash-reporter').start();
+var mainWindow = null;
+
+app.on('window-all-closed', function(){
+  if(process.platform != 'darwin'){
+    app.quit();
+  }
+});
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({width: 120, height: 80, transparent: true, frame: false});
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function(){
+    mainWindow = null;
+  });
+});
